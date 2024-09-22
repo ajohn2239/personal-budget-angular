5d691a6cdda9e9d168a4493150a950d3c61dd9c2
Aminah John <ajohn7@charlotte.edu>
2024-09-22 00:05:53 -0400

    Added code based on 05D coding video

---
+++ personal-budget/src/app/article/article.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pb-article',
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css'
+})
+export class ArticleComponent implements OnInit{
+
+  constructor(){ }
+  ngOnInit(): void {
+      
+  }
+}
